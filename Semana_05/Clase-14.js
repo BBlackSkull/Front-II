f357bf9b69a0ba0bfb11f6f43e1e01fc33832fbd
Yamall <yamall.avalos@gmail.com>
2023-03-22 23:17:43 -0300

    agregando semana 6

--- Semana_05/Clase-14.js
+++ Semana_05/Clase-14.js
@@ -1,9 +1,14 @@
 window.addEventListener('load', function(){
     console.log('Cargo la Pagina');
+
+    validarUsuario(); // Script de clase 15
     let usuario = localStorage.getItem('usuario');
 
     usuario = JSON.parse(usuario);
-    console.log(usuario);
+    //console.log(usuario);
+
+    renderizarDatosUsuario(usuario);
+    cerrarSesion();
 });
 
 
@@ -11,38 +16,52 @@ window.addEventListener('load', function(){
 // FUNCION PARA
 // 1. Con los dato obtenidos del localSorage mostrar los datos en DOM
 // 2. Crea un boton cerrar sesion el cual elimina los datos de localStorage
+function renderizarDatosUsuario(usuario){
+    const emailLabel = document.querySelector('#email');
+    const perfilLabel = document.querySelector('#perfil');
 
-function datosUsuarioDom(usuario){
-
-    let email = document.querySelector('#email');
-    let perfil = document-querySelector('#perfil');
-    console.log(email);
-
-    email.innerHTML += usuario.email;
-
-    perfil.innerHTML += usuario.rol
-
+    emailLabel.innerText = usuario.email;
+    perfilLabel.innerText = usuario.rol;
 }
 
-function botonCerrar(){
+/* function botonCerrarSesion(){
+    let div = document.querySelector(".user")
+    div.innerHTML+=
+    `
+    <button id="cerrar">Cerrar Sesión</button>
+    `;
+    let cerrar = document.querySelector("#cerrar");
 
-    let div = document.querySelector('.user');
 
-        div.innerHTML+= `
-        <button id="cerrar"> Cerrar Sesion </button>
+    cerrar.addEventListener('click', function(){
+        console.log('Se cerro la sesión');
+        //localStorage.removeItem("usuario");
+        //location.href = 'index.html';
+    });
+    
 
-        `
+} */
 
-        let cerrar = document.querySelector('#cerrar');
-        cerrar.addEventListener('click', function(){
-            localStorage.removeItem('usuario');
-            location.herf = 'index.html';
+function cerrarSesion(){
 
-            const confirmacion = confirm('Desea cerrar sesion')
-            if (confirmacion === true){
+    let user = document.querySelector('.user');
+        
+    const boton = //html
+    
+                `<button class="buttonCerrarSesion" style.color="green">Cerrar sesión</button>`;
+    
+        user.innerHTML += boton;
+        
+        const capturaBoton = document.querySelector('.buttonCerrarSesion');
+        capturaBoton.style= "background:rgba(255,0,0,0.2);padding:5px 20px;color: red;margin: 20px;border-style:none;cursor: pointer;";
+        capturaBoton.addEventListener("click", function (){
+            const confirmacion =confirm("¿Seguro desea cerrar sesión?");
+            if(confirmacion){
+            
                 localStorage.clear();
-                location.href = 'index.html'; 
+                //location.href = 'index.html';
+                location.replace('index.html');
             }
+            
         })
-
-}
+    }
